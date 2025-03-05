cdf9e271f95cc7eba9e1a5b9e01d9740873be746
navidras <navidras@gmail.com>
2025-03-05 11:59:04 +0330

    remove test files

--- GrpcService/Program.cs
+++ GrpcService/Program.cs
@@ -12,15 +12,12 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddGrpc();
 builder.Services.ConfigRiraService(builder.Configuration);
 builder.Services.AddAutoMapper(typeof(AutomapperProfile));
-//var typeAdapterConfig = TypeAdapterConfig.GlobalSettings;
-//typeAdapterConfig.Scan(Assembly.GetExecutingAssembly());
-//var mapperConfig = new Mapper(typeAdapterConfig);
-//builder.Services.AddSingleton<IMapper>(mapperConfig);
+
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-app.MapGrpcService<GreeterService>();
+
 app.MapGrpcService<PersonService>();
 app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
 
