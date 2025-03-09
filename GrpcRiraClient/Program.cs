a94e023a6dd74da0e80d106fb3b981b3750644f1
navidras <navidras@gmail.com>
2025-03-09 17:14:11 +0330

    grpc client

---
+++ GrpcRiraClient/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Grpc.Net.Client;
+using GrpcRiraTheTest;
+
+Console.WriteLine("Hello, World!");
+
+using var channel = GrpcChannel.ForAddress("http://localhost:5192");
+
+var person = new PersonCrud.PersonCrudClient(channel);
+
+var reply = await person.GetAllPersonsAsync(new Empty());
+
+foreach(var i in reply.Persons)
+{
+    Console.WriteLine(i.FirstName);
+}
+
+var personAddResult = person.AddPerson(new AddPersonRequestMessage() { BirthDate = 1, FirstName = "test2", LastName = "test3", NationlaCode = "1234567890" });
+
+
+Console.WriteLine("All persons", personAddResult.Message);
+
